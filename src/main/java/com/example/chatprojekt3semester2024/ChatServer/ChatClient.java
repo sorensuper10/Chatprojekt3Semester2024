3b0dcab5002bdd39a35e83faf4768583d7cd7249
sorensuper10 <soren_super@hotmail.com>
2024-09-19 14:54:24 +0200

    Version 1.8

--- src/main/java/com/example/chatprojekt3semester2024/ChatServer/ChatClient.java
+++ src/main/java/com/example/chatprojekt3semester2024/ChatServer/ChatClient.java
@@ -1,9 +1,6 @@
 package com.example.chatprojekt3semester2024.ChatServer;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 
 public class ChatClient {
@@ -49,7 +46,7 @@ public class ChatClient {
             System.out.println("File does not exist.");
 
         }
-        try (Socket fileSocket = new Socket("localhost", 5000);
+        try (Socket fileSocket = new Socket("192.168.0.100", 5000);
              FileInputStream fileInputStream = new FileInputStream(file);
              BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
              OutputStream outputStream = fileSocket.getOutputStream();
